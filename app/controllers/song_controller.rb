d15eaf5c8c6297754c374a915d96920c1dadf7fc
hrkd <hiroki@hrkd.net>
2015-04-26 21:30:51 +0900

    jsonp対応

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -1,7 +1,9 @@
 class SongController < ApplicationController
+    protect_from_forgery except: :view
+
     def view
         song = Song.new()
-        render :json => song.getSong(params[:station])
+        render :json => song.getSong(params[:station]), callback: params[:callback]
     end
 end
 
